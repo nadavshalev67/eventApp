bd8dd68883c7266f5cc19a9e7941945d65523054
Nadav Shalev <nadav.shalev@fyber.com>
2021-01-02 12:37:41 +0200

    blabla

--- app/src/main/java/com/example/eventapp/MainActivity.java
+++ app/src/main/java/com/example/eventapp/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_login_activity);
+        //Add comment
     }
 
 
