24fde0fd486cba8579a18c955fca3b342776627f
GUY <guygrumach@gmail.com>
2021-01-04 22:06:01 +0200

    add support to even add screen

--- app/src/main/java/com/example/eventapp/AddEventActivity.java
+++ app/src/main/java/com/example/eventapp/AddEventActivity.java
@@ -7,7 +7,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,58 +19,51 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 
-public class AddEventActivity extends Activity {
-    private EditText mEventName;
-    private EditText mEventDesc;
-    private EditText mEventAddress;
-    private Spinner mLevelOfRisk;
+public class AddEventActivity extends Activity implements View.OnClickListener {
+    private ImageView mAddPicBtn, mImageDescription;
+    private EditText mEventName, mEventDesc, mEventAddress;
+    private Spinner mLevelOfRiskSpinnner;
     private Button mAddBtn;
-    private ImageView mAddPicBtn;
+    private boolean mIsImageSeted = false;
+
     private static final int CAMERA_REQUEST = 1888;
-    private ImageView imageView;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private static final String[] ARRAY_SPINNER = new String[]{
+            "easy", "medium", "hard"
+    };
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_a_event);
-        imageView = findViewById(R.id.EventImage);
+        initViews();
+
+    }
+
+    private void initViews() {
+        mImageDescription = findViewById(R.id.EventImage);
         mEventName = findViewById(R.id.eventName);
         mEventDesc = findViewById(R.id.eventDesc);
-        mEventName = findViewById(R.id.eventAddress);
+        mEventAddress = findViewById(R.id.eventAddress);
         mAddBtn = findViewById(R.id.addBtn);
-
         mAddPicBtn = findViewById(R.id.AddPicBtn);
-        mAddPicBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-                        }
-                    } else {
-                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                    }
-                }
+        initSpinnner();
+        setOnCLickLisenterToMultipleViews(mAddPicBtn, mAddBtn);
+    }
 
+    private void setOnCLickLisenterToMultipleViews(View... views) {
+        for (int i = 0; i < views.length; i++) {
+            views[i].setOnClickListener(this);
+        }
+    }
 
-            }
-        });
-
-
-        String[] arraySpinner = new String[]{
-                "easy", "medium", "hard"
-        };
-        Spinner s = (Spinner) findViewById(R.id.levelOfRisk);
+    private void initSpinnner() {
+        mLevelOfRiskSpinnner = (Spinner) findViewById(R.id.levelOfRisk);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, arraySpinner);
+                android.R.layout.simple_spinner_item, ARRAY_SPINNER);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
-
-
+        mLevelOfRiskSpinnner.setAdapter(adapter);
     }
 
     @Override
@@ -79,8 +72,7 @@ public class AddEventActivity extends Activity {
         if (requestCode == MY_CAMERA_PERMISSION_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                openCamera();
             } else {
                 Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
             }
@@ -92,8 +84,68 @@ public class AddEventActivity extends Activity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+            mIsImageSeted = true;
+            mImageDescription.setImageBitmap(photo);
         }
     }
 
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.addBtn: {
+                String eventName;
+                String eventDescription;
+                String emailAdd;
+                String levelOfRisk;
+                String spinner;
+
+                if (TextUtils.isEmpty(mEventName.getText().toString())) {
+                    Toast.makeText(AddEventActivity.this, "please enter name", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                eventName = mEventName.getText().toString();
+
+                if (TextUtils.isEmpty(mEventDesc.getText().toString())) {
+                    Toast.makeText(AddEventActivity.this, "please enter description", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                eventDescription = mEventDesc.getText().toString();
+
+                if (TextUtils.isEmpty(mEventAddress.getText().toString())) {
+                    Toast.makeText(AddEventActivity.this, "please enter email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                emailAdd = mEventAddress.getText().toString();
+
+                String spin = mLevelOfRiskSpinnner.getSelectedItem().toString();
+
+
+                if (!mIsImageSeted) {
+                    Toast.makeText(AddEventActivity.this, "please take a photo", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                break;
+            }
+            case R.id.addPicBtn: {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                        }
+                    } else {
+                        openCamera();
+                    }
+                }
+
+                break;
+            }
+        }
+    }
+
+    private void openCamera() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
 }
--- app/src/main/java/com/example/eventapp/eventMain.java
+++ app/src/main/java/com/example/eventapp/eventMain.java
@@ -21,94 +21,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class eventMain extends AppCompatActivity {
 
-    private EditText mEventName;
-    private EditText mEventDesc;
-    private EditText mEventAddress;
-    private Spinner mLevelOfRisk;
-    private Button mAddBtn;
-    private ImageView mAddPicBtn;
-    private static final int CAMERA_REQUEST = 1888;
-    private ImageView imageView;
-    private static final int MY_CAMERA_PERMISSION_CODE = 100;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_main);
-        imageView = findViewById(R.id.EventImage);
-        mEventName = findViewById(R.id.eventName);
-        mEventDesc = findViewById(R.id.eventDesc);
-        mEventName = findViewById(R.id.eventAddress);
-        mAddBtn = findViewById(R.id.addBtn);
-
-        mAddPicBtn = findViewById(R.id.AddPicBtn);
-        mAddPicBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-                    {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-                        }
-                    }
-                    else
-                    {
-                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-                    }
-                }
-
-
-            }
-        });
-
-
-
-        String[] arraySpinner = new String[] {
-                "easy", "medium", "hard"
-        };
-        Spinner s = (Spinner) findViewById(R.id.levelOfRisk);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, arraySpinner);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
-
-
-
-
-
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_PERMISSION_CODE)
-        {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            {
-                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            }
-            else
-            {
-                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
-        }
     }
 }
 
