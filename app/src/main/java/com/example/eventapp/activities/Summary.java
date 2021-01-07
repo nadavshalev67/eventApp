3d1b88626800eac22b21d94234816c05f307116d
GUY <guygrumach@gmail.com>
2021-01-07 23:49:39 +0200

    add support to even add screen

--- app/src/main/java/com/example/eventapp/activities/Summary.java
+++ app/src/main/java/com/example/eventapp/activities/Summary.java
@@ -10,10 +10,12 @@ import com.example.eventapp.database.SQLBase;
 import com.example.eventapp.database.SQLHolder;
 import com.example.eventapp.database.firestore.FireStoreSql;
 import com.example.eventapp.models.Event;
+import com.example.eventapp.models.User;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.List;
 
-public class Summary extends Activity implements SQLBase.SqlListenerEvents {
+public class Summary extends Activity implements SQLBase.SqlListenerEvents , SQLBase.SqlListenerUsers {
 
 
     EditText mMyEvents, mApproved, mRejected,
@@ -27,11 +29,12 @@ public class Summary extends Activity implements SQLBase.SqlListenerEvents {
         setContentView(R.layout.summery);
         init();
         SQLHolder.getInstance().getAllEvents(this);
+        SQLHolder.getInstance().getAllUsers(this);
     }
 
     public void init() {
         mMyEvents = findViewById(R.id.myevent_edittext);
-        mApproved = findViewById(R.id.approve_count_user);
+        mApproved = findViewById(R.id.approved_edittext);
         mRejected = findViewById(R.id.rejected_edittext);
         mProfitFromEvent = findViewById(R.id.TotalProfit_edittext);
         mProfitFromApperoved = findViewById(R.id.TotalApproved_edittext);
@@ -64,6 +67,49 @@ public class Summary extends Activity implements SQLBase.SqlListenerEvents {
             mLevelM.setText(String.valueOf(medium));
             mLevelH.setText(String.valueOf(hard));
         }
+
+    }
+
+    @Override
+    public void onGetAllUsers(List<User> users) {
+
+        int numOfEvents = 0 ;
+        String email = "" ;
+        int maxTotal = 0 ;
+        String emailMax = "" ;
+
+        for (User user : users){
+            if(Integer.parseInt(user.amout_of_my_events) > numOfEvents) {
+                numOfEvents = Integer.parseInt(user.amout_of_my_events);
+                email = user.email;
+
+            }
+
+            if(Integer.parseInt(user.coins_aprroved_by_other_users_to_my_events) >= maxTotal){
+                maxTotal = Integer.parseInt(user.coins_aprroved_by_other_users_to_my_events);
+                emailMax = user.email;
+            }
+
+
+            if(user.uuid.equals(FirebaseAuth.getInstance().getUid())){
+                mMyEvents.setText(user.amout_of_my_events);
+                mApproved.setText(user.amount_approved_by_me);
+                mRejected.setText(user.amount_rejected_by_me);
+                mProfitFromEvent.setText(user.coins_aprroved_by_other_users_to_my_events);
+                mProfitFromApperoved.setText(user.conis_from_my_approve_to_other_user);
+
+
+
+            };
+        }
+        mMaxEvent.setText(String.valueOf(numOfEvents));
+        mUser.setText(email);
+        mMaxTotalT.setText(String.valueOf(maxTotal));
+        mUserSec.setText(emailMax);
+
+
+
+
 
     }
 }
